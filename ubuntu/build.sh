23fecb6e29cbc8500c7e8317aaad26432c4757bd
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-28 02:14:27 +0800

    Update build.sh

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -4,3 +4,25 @@ cd ./packaging
 mkdir -p renv-pkg/DEBIAN
 mkdir -p renv-pkg/opt/renv/bin
 
+/bin/cp -rf /mnt/renv/net8.0/* renv-pkg/opt/renv/bin/
+
+cat "Package: rsharp
+Version: 1.0.0
+Section: utils
+Priority: optional
+Architecture: amd64
+Depends: dotnet-sdk-8.0 (>= 8.0.110),
+         libc6 (>= 2.39),
+         libgcc-s1 (>= 14.2.0),
+         libgssapi-krb5-2 (>= 1.21.3),
+         libicu74 (>= 74.2),
+         libssl3 (>= 3.0.13),
+         libstdc++6 (>= 14.2.0),
+         tzdata (>= 2024a),
+         zlib1g (>= 1.3)
+Maintainer: xieguigang <your.email@example.com>
+Description: My custom R environment tools
+  This package contains a set of command-line tools for managing
+  a specific R environment. It installs the necessary executables
+  into /opt/renv/bin.
+" > renv-pkg/DEBIAN/control
