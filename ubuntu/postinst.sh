a72c9301ec27a04bb03e331c808e0a2d19df50f8
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-28 02:52:54 +0800

    add enrivuonent path

--- ubuntu/postinst.sh
+++ ubuntu/postinst.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+PROFILE_D_FILE="/etc/profile.d/rsharp.sh"
+
 case "$1" in
     configure)
 
@@ -11,6 +13,15 @@ case "$1" in
         dotnet /opt/renv/bin/R#.dll /linux-bash
 
         chmod +x /opt/renv/bin/R#
+
+        echo "  -> add /opt/renv/bin into system environment PATH..."
+        cat > "${PROFILE_D_FILE}" <<'EOF'
+# Renvironmnet tools path configuration
+# This file is managed by the renv-tools package.
+if [ -d "/opt/renv/bin" ]; then
+    export PATH="/opt/renv/bin:$PATH"
+fi
+EOF
 
         R# --install.packages /opt/renv/packages/REnv.zip
 
