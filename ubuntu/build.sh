850b6bda7ff55b10d94d64c6e0296e7e707561a4
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-28 02:31:40 +0800

    split source file into multiple parts

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 mkdir -p /tmp/renv_packaging
 cd /tmp/renv_packaging
 
@@ -6,19 +8,7 @@ mkdir -p renv-pkg/opt/renv/bin
 
 /bin/cp -rf /mnt/renv/net8.0/* renv-pkg/opt/renv/bin/
 
-cat > renv-pkg/DEBIAN/control << EOF
-Package: rsharp
-Version: 1.0.0
-Section: utils
-Priority: optional
-Architecture: amd64
-Depends: dotnet-sdk-8.0 (>= 8.0.110)
-Maintainer: xieguigang <xieguigang@metabolomics.ac.cn>
-Description: R# runtime environment 
-  This package contains a set of command-line tools for managing
-  a specific R environment. It installs the necessary executables
-  into /opt/renv/bin.
-EOF 
+cat /mnt/renv/metadata.txt > renv-pkg/DEBIAN/control 
 
 dpkg-deb --build renv-pkg
 mv renv-pkg.deb renv_1.0.0_amd64.deb
