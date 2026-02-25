d59ecaf0db04d098857256eb5d425865722a6625
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-25 11:26:18 +0800

    configs of the package installer builder

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -22,6 +22,6 @@ chmod +x renv-pkg/DEBIAN/postinst
 chmod +x renv-pkg/DEBIAN/postrm
 
 dpkg-deb --build renv-pkg
-/bin/cp -rf renv-pkg.deb /mnt/renv/renv_1.0.0_amd64.deb
+/bin/cp -rf renv-pkg.deb /mnt/renv/renv_1.2.10_amd64.deb
 
-# dpkg -i /mnt/renv/renv_1.0.0_amd64.deb
+# dpkg -i /mnt/renv/renv_1.2.10_amd64.deb
